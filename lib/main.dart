a543dd536153a1ecae18afb8f827a9f6c865ab3a
Hendrikson Petzold <hendrikson_14@hotmail.com>
2022-08-05 15:18:54 -0300

    fix: status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:sweets_store_app/app.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(const App());
 }
