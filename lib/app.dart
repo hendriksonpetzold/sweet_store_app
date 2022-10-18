e1df8150afdc7215df95992e93d3b535c6bf60f2
Hendrikson Petzold <hendrikson_14@hotmail.com>
2022-10-18 11:20:56 -0300

    refact: debug banner mode

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Sweet Store App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
